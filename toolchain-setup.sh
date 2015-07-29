c0cbbd1e562822fc986afe1d87acccb095fdbb97
Emilio Jesus Gallego Arias <e+git@x80.org>
2015-07-29 19:45:42 +0200

    Minor.

--- toolchain-setup.sh
+++ toolchain-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# OCAML_VER=4.02.3
+OCAML_VER=4.02.1
 NJOBS=32
 JS_OF_OCAML_DIR=~/external/js_of_ocaml
 
-JSCOQ_DEPS=ocamlfind camlp4 camlp5 base64 cppo ppx_tools higlo ocp-indent tyxml js_of_ocaml reactiveData yojson
+JSCOQ_DEPS="ocamlfind camlp4 camlp5 base64 cppo ppx_tools higlo ocp-indent tyxml js_of_ocaml reactiveData yojson"
 
 do_setup() {
   opam install -y -j $NJOBS $JSCOQ_DEPS
@@ -12,11 +14,11 @@ do_setup() {
   popd
 }
 
-opam switch -j $NJOBS -y 4.02.1
+opam switch -j $NJOBS -y $OCAML_VER
 eval `opam config env`
 do_setup
 
-opam switch -j $NJOBS -y 4.02.1+32bit
+opam switch -j $NJOBS -y $OCAML_VER+32bit
 eval `opam config env`
 do_setup
 
