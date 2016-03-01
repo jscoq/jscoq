478c99628622acf1e9617298ecd6cd282f497e14
Benoît Pin <pin@cri.ensmp.fr>
2016-03-01 13:14:43 +0100

    + Gruntfile to concatenate js libs dependencies.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+    // grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.initConfig({
+        // wiredep: {
+        //     target: {
+        //         src: 'panelside.html'
+        //     }
+        // },
+        
+        bower_concat: {
+            all: {
+                dest: {
+                    'js': 'js/_bower.js',
+                    'css': 'css/_bower.css'
+                }
+            }
+        }
+    });
+};
