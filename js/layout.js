04d2bbdba90a7aeb89003309ded85c8e1d6ee37f
Benoît Pin <pin@cri.ensmp.fr>
2016-03-02 18:10:53 +0100

    Margins adjusts.

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,11 @@
 (function () {
     var options = {
-        //height: 5,
+        //height: 10,
         //cell_height: document.documentElement.clientHeight / 4,
         resizable: {
             handles: 'e, se, s, sw, w'
         },
+        vertical_margin: 3,
         //handle: '.jscoq-grid-stack-handle',
     };
     var grid = $('.grid-stack').gridstack(options);
