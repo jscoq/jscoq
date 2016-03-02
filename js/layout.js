387dcbb23d7132cfff73029c52cd661a5d78feab
Benoît Pin <pin@cri.ensmp.fr>
2016-03-02 17:06:57 +0100

    Grid layout first try.

---
+++ js/layout.js
@@ -0,0 +1,12 @@
+(function () {
+    var options = {
+        //height: 5,
+        //cell_height: document.documentElement.clientHeight / 4,
+        resizable: {
+            handles: 'e, se, s, sw, w'
+        },
+        //handle: '.jscoq-grid-stack-handle',
+    };
+    var grid = $('.grid-stack').gridstack(options);
+
+}());
