5cd2552065012c07530ae0768d2dfb28acca8b3e
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-06-30 15:17:26 +0200

    [build] Tweaks on version management.

--- config.mk
+++ config.mk
@@ -1,7 +1,14 @@
 # Jobs to use
 JOBS=8
 
-COQ_VERSION=v8.7
+COQ_VERSION:=v8.7
+# JSCOQ_BRANCH:=js-worker
+
+JSCOQ_VERSION:=$(COQ_VERSION)
+
+ifdef JSCOQ_BRANCH
+JSCOQ_VERSION:=$(JSCOQ_VERSION)-$(JSCOQ_BRANCH)
+endif
 
 # Directory where the coq sources are
 COQDIR=~/external/coq-$(COQ_VERSION)+32bit/
@@ -15,11 +22,11 @@ ADDONS =
 
 # Have in every add-on a make jscoq target? We could certainly
 # classify coq_makefile ones.
-ADDONS_PATH=/home/egallego/external/coq
+ADDONS_PATH:=/home/egallego/external/coq
 
 # Custom stdlib for hott
-HOTT_COQLIB=$(ADDONS_PATH)/HoTT/coq/theories/
+HOTT_COQLIB:=$(ADDONS_PATH)/HoTT/coq/theories/
 
 RELEASE_DIR=~/research/jscoq-builds/
-WEB_DIR=~/x80/rhino-coq/$(COQ_VERSION)/
+WEB_DIR=~/x80/jscoq-builds/$(JSCOQ_VERSION)/
 HOTT_RELEASE=~/x80/rhino-hott/
