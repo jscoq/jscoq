f472c44d12c8fe16b098a8d81ac6a5abb95c7a18
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-11-26 21:22:58 +0100

    [addons] Prepare for Coq and addons to be built by jsCoq scripts.

--- config.mk
+++ config.mk
@@ -10,11 +10,15 @@ ifdef JSCOQ_BRANCH
 JSCOQ_VERSION:=$(JSCOQ_VERSION)-$(JSCOQ_BRANCH)
 endif
 
-# Directory where the coq sources are
-COQDIR=~/external/coq-$(COQ_VERSION)+32bit/
+# ugly but I couldn't find a better way
+current_dir := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+# Directory where the coq sources and developments are.
+ADDONS_PATH := $(current_dir)/coq-external
+COQDIR := $(ADDONS_PATH)/coq-$(COQ_VERSION)+32bit/
 
 # Addons to build
-ADDONS =
+ADDONS = ssr-libs
 # Woking on coq-8.6:
 # ADDONS += ssr-libs coquelicot flocq sf cpdt dsp hott color
 # Woking on coq-8.5:
@@ -22,11 +26,11 @@ ADDONS =
 
 # Have in every add-on a make jscoq target? We could certainly
 # classify coq_makefile ones.
-ADDONS_PATH:=/home/egallego/external/coq
 
 # Custom stdlib for hott
 HOTT_COQLIB:=$(ADDONS_PATH)/HoTT/coq/theories/
 
+# Private paths, for releases and local builds.
 RELEASE_DIR=~/research/jscoq-builds/
 WEB_DIR=~/x80/jscoq-builds/$(JSCOQ_VERSION)/
 HOTT_RELEASE=~/x80/rhino-hott/
