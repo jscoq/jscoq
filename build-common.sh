7bb33145bfedcb1b66c64b36845de0fdc3c46130
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-11-29 18:31:42 +0100

    [build] Switch to 4.06.0 + js_of_ocaml 3.0.2.

--- build-common.sh
+++ build-common.sh
@@ -1,4 +1,4 @@
 # Common variables
 
-OCAML_VER=4.04.2
+OCAML_VER=4.06.0
 NJOBS=4
--- toolchain-setup.sh
+++ toolchain-setup.sh
@@ -10,12 +10,17 @@ VERB=
 # VERB=-vv
 
 do_setup() {
-  opam install -y $VERB -j $NJOBS $JSCOQ_DEPS
-  # JsCoq runs now wtih js_of_ocaml OPAM
-  # git clone https://github.com/ocsigen/js_of_ocaml.git ~/external/js_of_ocaml
-  # pushd $JS_OF_OCAML_DIR
-  # make clean && make -j $NJOBS && make uninstall install
-  # popd
+
+  jsoo_repo=https://github.com/ocsigen/js_of_ocaml.git
+
+  # Pinning of js_of_ocaml
+  # for pkg in js_of_ocaml{-ppx,-lwt,-compiler,}
+  # do
+  #   opam pin add -y ${VERB} ${pkg} ${jsoo_repo}
+  # done
+
+  opam install -y ${VERB} -j ${NJOBS} ${JSCOQ_DEPS}
+
 }
 
 #opam switch -j $NJOBS -y $OCAML_VER
