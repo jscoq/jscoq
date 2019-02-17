bd7d438c476e8cbcb8814f46deb4e3ff720b6401
Shachar Itzhaky <corwin.amber@gmail.com>
2019-02-17 14:37:59 +0200

    [feature] Tiny utility to package bundles in zip files.

---
+++ coq-tools/mkpkg.js
@@ -0,0 +1,58 @@
+/**
+ * Used to create a zip bundle out of a JSON files that
+ * contains a list of .vo and .cmo filenames.
+ */
+
+const fs = require('fs'),
+      path = require('path'),
+      JSZip = require('jszip');
+
+class PackageDefinition {
+    constructor(manifest, base_path) {
+        if (typeof manifest == 'string') {
+            this.manifest = JSON.parse(fs.readFileSync(manifest, 'utf-8'));
+            this.base_path = base_path || path.dirname(manifest);
+        }
+        else {
+            this.manifest = manifest;
+            this.base_path = base_path; // a required argument in this case
+        }
+    }
+
+    listFiles() {
+        var files = [];
+        for (let pkg of this.manifest.pkgs) {
+            for (let file_entry of (pkg.vo_files || []).concat(pkg.cma_files || [])) {
+                files.push(path.join(...pkg.pkg_id, file_entry[0]));
+            }
+        }
+        return files;
+    }
+
+    toZip(save_as) {
+        var z = new JSZip();
+        z.file("coq-pkg.json", JSON.stringify(this.manifest, null, 2));
+        for (let fn of this.listFiles()) {
+            let phys = path.join(this.base_path, fn);
+            z.file(fn, fs.createReadStream(phys)
+                .on("error", () => console.log(`error reading '${phys}'.`)));
+        }
+        if (save_as) {
+            return z.generateNodeStream()
+                .pipe(fs.createWriteStream(save_as))
+                .on('finish', () => console.log(`wrote '${save_as}'.`));
+        }
+        else
+            return z;
+    }
+}
+
+// Usage:
+//  node mkpkg.js <json filenames...>
+if (module.id == '.') {
+    for (let json_fn of process.argv.slice(2)) {
+        var pd = new PackageDefinition(json_fn),
+            zip_fn = json_fn.replace(/([.]json|)$/, '.zip');
+        pd.toZip(zip_fn);
+    }
+}
