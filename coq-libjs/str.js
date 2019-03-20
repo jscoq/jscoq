1f912c1f093a50080701adf8c2b94c507d4920ba
Shachar Itzhaky <corwin.amber@gmail.com>
2019-03-20 23:06:36 +0200

    [bugfix] Restored logging of Str primitives.
    Made sure that primitive stubs return valid OCaml values.

--- coq-libjs/str.js
+++ coq-libjs/str.js
@@ -1,4 +1,4 @@
-function str_ll(s) { if (v_log) console.warn(s); }
+function str_ll(s, args) { if (str_ll.log) console.warn(s, args); }
 str_ll.log = true;
 
 //Provides: str_ll
@@ -9,7 +9,7 @@ var str_ll;
 function re_string_match() {
   // external re_string_match : regexp -> string -> int -> bool
   if (!re_string_match.warned) {
-    str_ll("Danger: missing primitive re_string_match called from Str");
+    str_ll("(from Str) re_string_match", arguments);
     re_string_match.warned = true;
   }
   return false;
@@ -20,18 +20,21 @@ function re_string_match() {
 function re_search_forward() {
   // external re_search_forward: regexp -> string -> int -> int array
   if (!re_search_forward.warned) {
-    str_ll("Danger: missing primitive re_search_forward called from Str");
+    str_ll("(from Str) re_search_forward", arguments);
     re_search_forward.warned = true;
   }
-  return [];
+  return [0]; /* [||] : int array */
 }
 
 //Provides: re_partial_match
 //Requires: str_ll
-function re_partial_match()          { str_ll('re_partial_match', arguments); }
+// external re_partial_match: regexp -> string -> int -> int array
+function re_partial_match()          { str_ll('re_partial_match', arguments);     return [0]; }
 //Provides: re_replacement_text
 //Requires: str_ll
-function re_replacement_text()       { str_ll('re_replacement_text', arguments); }
+// external re_replacement_text: string -> int array -> string -> string
+function re_replacement_text(_,_,s)  { str_ll('re_replacement_text', arguments);  return s; }
 //Provides: re_search_backward
 //Requires: str_ll
-function re_search_backward()        { str_ll('re_search_backward', arguments); }
+// external re_search_backward: regexp -> string -> int -> int array
+function re_search_backward()        { str_ll('re_search_backward', arguments);   return [0]; }
