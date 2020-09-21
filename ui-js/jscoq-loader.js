4d8a4aa6999e417868f3cfcecc62abe8327a8e3f
Shachar Itzhaky <corwin.amber@gmail.com>
2020-09-21 20:13:53 +0300

    [cleanup] Proper use of async/await and clean separation of load and start.

--- ui-js/jscoq-loader.js
+++ ui-js/jscoq-loader.js
@@ -11,15 +11,13 @@
 "use strict";
 
 var loadJs = function(js) {
-    return function () {
-        return new Promise(function (resolve, error) {
-            var script    = document.createElement('script');
-            script.type   = 'text/javascript';
-            script.src    = js + '.js';
-            script.onload = resolve;
-            document.head.appendChild(script);
-        });
-    };
+    return new Promise(function (resolve, error) {
+        var script    = document.createElement('script');
+        script.type   = 'text/javascript';
+        script.src    = js + '.js';
+        script.onload = resolve;
+        document.head.appendChild(script);
+    });
 };
 
 var loadJsCoq, JsCoq;
@@ -48,7 +46,7 @@ var loadJsCoq, JsCoq;
     // - localForage
     // - jsCoq = cm-provider + coq-packages + coq-manager
 
-    loadJsCoq = function(base_path, node_modules_path) {
+    loadJsCoq = async function(base_path, node_modules_path) {
 
         base_path = (base_path || JsCoq.base_path).replace(/([^/])$/, '$1/');
         JsCoq.base_path = base_path;
@@ -87,22 +85,22 @@ var loadJsCoq, JsCoq;
                      base_path + 'ui-js/coq-layout-classic',
                      base_path + 'ui-js/coq-manager'];
 
-        return files.reduce(function(prom, file) {
-            return prom.then(loadJs(file));
-        }, Promise.resolve());
+        for (let js of files) await loadJs(js);
     };
 
     JsCoq = {
         base_path: scriptDir ? `${scriptDir}../` : "./",
         node_modules_path: '',
+        loaded: undefined,
 
         is_npm: false,  /* indicates that jsCoq was installed via `npm install` */
 
         load(base_path, node_modules_path) {
-            return loadJsCoq(base_path, node_modules_path);
+            return this.loaded ||
+                (this.loaded = loadJsCoq(base_path, node_modules_path));
         },
 
-        start(...args) {
+        async start(...args) {
             var base_path = undefined, node_modules_path = undefined,
                 jscoq_ids = ['ide-wrapper'], jscoq_opts = {};
             
@@ -117,9 +115,8 @@ var loadJsCoq, JsCoq;
             jscoq_opts.base_path = jscoq_opts.base_path || base_path || JsCoq.base_path;
             base_path = base_path || jscoq_opts.base_path || JsCoq.base_path;
 
-            return this.load(base_path, node_modules_path).then(() =>
-                new CoqManager(jscoq_ids, jscoq_opts)
-            );
+            await this.load(base_path, node_modules_path);
+            return new CoqManager(jscoq_ids, jscoq_opts)
         }
     };
 
