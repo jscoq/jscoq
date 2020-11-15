e15792585593b75b83cd625af73f4b3e7b783ec2
Shachar Itzhaky <corwin.amber@gmail.com>
2020-11-15 14:13:06 +0200

    [addons] Updated affiliated package list.

--- ui-js/coq-manager.js
+++ ui-js/coq-manager.js
@@ -1286,7 +1286,8 @@ const PKG_ALIASES = {
 const PKG_AFFILIATES = [  // Affiliated packages in @jscoq scope
     'mathcomp', 'elpi', 'equations', 'extlib', 'simpleio', 'quickchick', 
     'software-foundations',
-    'hahn', 'paco', 'snu-sflib', 'promising'
+    'hahn', 'paco', 'snu-sflib', 'promising',
+    'fcsl-pcm', 'htt', 'pnp'
 ];
 
 
