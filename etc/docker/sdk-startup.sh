0bcd09e68176bb693a8291375f3b27e959771eac
Shachar Itzhaky <corwin.amber@gmail.com>
2022-01-07 17:23:32 -0500

    [bugfix] [docker] Need to upgrade apt before installing.
    In case the certificates in the Debian image are too old, blocking
    installation of OPAM via wget.

--- etc/docker/sdk-startup.sh
+++ etc/docker/sdk-startup.sh
@@ -5,12 +5,12 @@ libgmp10:i386 libc6-i386
 wget ca-certificates
 )
 
-if [ ! -e .sdk-startup ] ; then
+if [ ! -e $HOME/.sdk-startup ] ; then
 dpkg --add-architecture i386
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -yq ${PKGS[@]}
 
-touch .sdk-startup
+touch $HOME/.sdk-startup
 fi
 
 exec "${@:-bash}"
