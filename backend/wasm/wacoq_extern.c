402b074802520e5d0aa170e2ecdf3e249837d8d1
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-10-18 13:29:33 +0200

    [wacoq] Make interrupt setup go thru the interpreter
    This is with the hope to unify the package manager between the two
    backends, and in particular to let the interpreter avoid the
    round-trip to the client for the auto-require functionality.
    
    That should make much easier to support the auto-loading in the lsp
    backend.

--- backend/wasm/wacoq_extern.c
+++ backend/wasm/wacoq_extern.c
@@ -3,6 +3,7 @@
 
 // Implemented in core.ts
 value interrupt_pending() { return Val_false; }
+value interrupt_setup(value opaque) { return Val_false; }
 
 // For subproc mode
 value wacoq_emit(value str) { printf("%s\n", (const char *)str); fflush(stdout); return Val_unit; }
