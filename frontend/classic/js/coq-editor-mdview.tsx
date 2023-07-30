16a29e46815f47afcbfd7e0ed3a4b764343857aa
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-07-30 18:18:47 +0200

    wip

--- frontend/classic/js/coq-editor-mdview.tsx
+++ frontend/classic/js/coq-editor-mdview.tsx
@@ -7,18 +7,26 @@ import { CoqDocument } from "./coq-manager";
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 import 'katex/dist/katex.css';
+import { CoqCodeMirror5 } from "./coq-editor-cm5";
 
 export class CoqMdViewEditor implements ICoqEditor {
     root : Root;
+    editor : CoqCodeMirror5;
 
     constructor(doc : CoqDocument, options)  {
         this.root = createRoot(doc.container);
         this.root.render(<MarkdownPreview source={doc.area.value}
             remarkPlugins={[remarkMath]}
             rehypePlugins={[rehypeKatex]} />);
+
+        // Replace this by emitting our own textarea (hidden?)
+        let ids = doc.container.querySelectorAll('.copied');
+        this.editor = new CoqCodeMirror5(doc, options)
     }
+
     destroy(): void {
         this.root.unmount();
+        this.editor.destroy();
     }
     getValue(): string {
         return "";
