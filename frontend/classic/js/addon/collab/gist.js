81ee4fecf1dd50952ee38295812737fb0682eb61
Eladkay <eladkay@gmail.com>
2024-09-24 18:10:53 +0300

    Updated token

--- frontend/classic/js/addon/collab/gist.js
+++ frontend/classic/js/addon/collab/gist.js
@@ -1,7 +1,7 @@
 import {Octokit} from "@octokit/core";
 function getGithubToken() {
     const tokens = [
-        atob("Z2l0aHViX3BhdF8xMUJBRzIzTkkwOERoSmQ1Ym9Za3Q5X09Gb293ajR4b0huZ1dQMzNuczRIbUNBZ0V2eTJVNTR4MWU3QkNNWE5nbG5IN1JaSjNFQmhLbEtaU3pm")
+        atob("Z2hwXzM3WnBoNEU2MWRETFNVRDNTbERpa3hEaWNWQjlkMjRYdlQwNQ==")
     ];
     return tokens[Math.floor(Math.random() * tokens.length)];
 }
